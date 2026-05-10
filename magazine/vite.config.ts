7d6094e2d96d1bace259c192c304b3e2b978d96a
smalllqiang <F11F10E8@outlook.com>
2026-05-10 23:06:17 +0800

    feat: 嘗試添加部署

--- magazine/vite.config.ts
+++ magazine/vite.config.ts
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
-  resolve: {
-    alias: {
-      '@': '/src',
+    plugins: [vue(), UnoCSS()],
+    base: '/HITWITH-GUIDE/',
+    resolve: {
+        alias: {
+            '@': '/src',
+        },
     },
-  },
 })
